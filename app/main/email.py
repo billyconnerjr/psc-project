bb7f3eefe1be3b4d339dfc7b0fa58c4c3e31baae
Billy Conner <billychaos@gmail.com>
2020-03-05 11:52:59 -0500

    update email settings

--- app/main/email.py
+++ app/main/email.py
@@ -1,9 +1,9 @@
-from flask import render_template
+from flask import render_template, current_app
 from app.email import send_email
 
 def email_book_list(user, recipient_email, books):
     send_email('Someone has shared their Book Center list with you!',
-    sender = current_app.config['ADMINS'],
+    sender = current_app.config['ADMINS'][0],
     recipients=[recipient_email], 
     text_body=render_template('email/email_list.txt', user=user, books=books),
     html_body=render_template('email/email_list.html', user=user, books=books))
