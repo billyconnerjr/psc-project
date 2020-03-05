4a5a470dda2ce634045f95fe726892c088a0967e
Billy Conner Jr <billychaos@gmail.com>
2020-03-05 12:09:10 -0500

    Update email.py

--- app/main/email.py
+++ app/main/email.py
@@ -3,7 +3,7 @@ from app.email import send_email
 
 def email_book_list(user, recipient_email, books):
     send_email('Someone has shared their Book Center list with you!',
-    sender = current_app.config['ADMINS'][0],
+    sender = current_app.config['ADMINS'],
     recipients=[recipient_email], 
     text_body=render_template('email/email_list.txt', user=user, books=books),
     html_body=render_template('email/email_list.html', user=user, books=books))
